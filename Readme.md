51b47bf11c0516045a189181fd8fe3b435d4f9b0
Pankaj <pankajbeniwal3112@gmail.com>
2024-03-02 15:10:36 +0530

    add route for second assignment

---
+++ Readme.md
@@ -0,0 +1,2 @@
+<!-- server https://task-manager-be-pink.vercel.app 
+client https://task-manager-application-gilt.vercel.app -->
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 const cors = require("cors");
 require("./db");
+const products = require("./db/data.json");
 
 // Middleware
 app.use(express.json());
@@ -10,6 +11,10 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+app.get("/get-ecommerce-products", (req, res) => {
+	return res.status(200).json(products);
+});
+
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/tasks", require("./routes/tasks"));
 
